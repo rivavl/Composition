fba87b9728d4c524ed6d1020be0be616945ced29
rivavl <mvkalinenkova@yandex.ru>
2022-03-23 01:24:15 +0300

    start

---
+++ app/src/main/java/com/marina/composition/presentation/WelcomeFragment.kt
@@ -0,0 +1,17 @@
+package com.marina.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
