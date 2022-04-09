cc2dbc03208c6dabbce12da5870c33376adef14a
rivavl <mvkalinenkova@yandex.ru>
2022-04-09 16:56:09 +0300

    79. Реализация GameViewModel

--- app/src/main/java/com/marina/composition/domain/entity/Question.kt
+++ app/src/main/java/com/marina/composition/domain/entity/Question.kt
@@ -1,7 +1,10 @@
 package com.marina.composition.domain.entity
 
 data class Question(
-    val sun: Int,
+    val sum: Int,
     val visibleNumber: Int,
     val options: List<Int>
-)
+) {
+    val rightAnswer: Int
+        get() = sum - visibleNumber
+}
